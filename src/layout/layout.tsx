26c79a6e8ad18f9decde8bde4d97174bf4cc9919
pizarrosch <bayenrshc89@gmail.com>
2023-10-14 01:27:02 +0200

    Main page: Create footer

--- src/layout/layout.tsx
+++ src/layout/layout.tsx
@@ -3,25 +3,39 @@ import Link from "next/link";
 // import Navigation from "@/components/Navigation/Navigation";
 import {PropsWithChildren} from "react";
 import Image from "next/image";
-import logo from '../../public/assets/logo.svg';
+import headerLogo from '../../public/assets/logo.svg';
+import footerLogo from '../../public/assets/footer-logo.svg';
 import Navigation from "@/components/Navigation/Navigation";
 import s from './layout.module.scss';
 
 export default function Layout({children}: PropsWithChildren) {
     return (
         <>
-            <Head>
-                <title>Main Page</title>
-                <meta name="description" content="SkillFactory Next.js project" />
-                <meta name="viewport" content="width=device-width, initial-scale=1" />
-            </Head>
-            <header className={s.header}>
-                <Link href='/'>
-                    <Image src={logo} alt='company logo' />
-                </Link>
-                <Navigation />
-            </header>
-            <main>{children}</main>
+            <div className={s.root}>
+                <Head>
+                    <title>Main Page</title>
+                    <meta name="description" content="SkillFactory Next.js project"/>
+                    <meta name="viewport" content="width=device-width, initial-scale=1"/>
+                </Head>
+                <header className={s.header}>
+                    <Link href='/'>
+                        <Image src={headerLogo} alt='company logo'/>
+                    </Link>
+                    <Navigation/>
+                </header>
+                <main>{children}</main>
+            </div>
+            <footer className={s.footer}>
+                <Image src={footerLogo} alt='company logo'/>
+                <div className={s.contactsDataContainer}>
+                    <div className={s.contactsData}>
+                        <span>г. Москва, Цветной б-р, 40</span>
+                        <span>+7 495 771 21 11</span>
+                        <span>info@skan.ru</span>
+                    </div>
+                    <span className={s.copyright}>Copyright. 2022</span>
+                </div>
+            </footer>
         </>
     )
 }
